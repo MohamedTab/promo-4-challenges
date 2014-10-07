2ed0fdd6149579c62930677370d223de3efcc9b2
MohamedTab <m.tabib@live.fr>
2014-10-07 16:24:01 +0200

    exo5

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,9 @@
 require_relative "coach_answer"
-# TODO: Implement the program that makes you discuss with your coach from the terminal.
+# TODO: Implement the program that makes you discuss with your coache terminal.
+puts "comment ca va?"
+my_message = gets.chomp
+
+while my_message != ""
+  puts coach_answer(my_message)
+  my_message = gets.chomp
+end
