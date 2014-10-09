2fc388fb7db91f49b779e7a9fa6cd9885fc434dd
MohamedTab <m.tabib@live.fr>
2014-10-09 16:45:12 +0200

    exo enumerable & block_gym

--- 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
@@ -1,34 +1,60 @@
 def sum_odd_indexed(array)
   # TODO: computes the sum of elements at odd indices (1, 3, 5, 7, etc.)
   #       You should make use Enumerable#each_with_index
+   sum = 0
+    array.each_with_index do |e, i|
+       if i = i%2 != 0
+       sum+=e
+     end
+    end
+return sum
 end
 
 def even_numbers(array)
   # TODO: Return the even numbers from a list of integers.
   #       You should use Enumerable#select
+  array.select do |number|
+    number%2 == 0
+  end
 end
 
 def short_words(array, max_length)
   # TODO: Take and array of words, return the array of words not exceeding max_length characters
   #       You should use Enumerable#reject
+  max_length = 4
+  array.reject do |a|
+    a.length > max_length
+  end
+
 end
 
 def first_under(array, limit)
   # TODO: Return the first number from an array that is less than limit.
   #       You should use Enumerable#find
+  array.find do |a|
+    a < limit
+end
 end
 
 def add_bang(array)
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
+  array.map do |a|
+    "#{a}!"
+  end
 end
 
 def product(array)
   # TODO: Calculate the product of an array of numbers.
   #       You should use of Enumerable#reduce
+  array.reduce do |a, i|
+    result = a * i
+  end
 end
 
 def sorted_pairs(array)
   # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
   #       You should make use of Enumerable#each_slice
+   array.each_slice(2).to_a.map { |value| value.sort }
 end
+
--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,8 +1,32 @@
 def tag(tag_name, attr = nil)
   # TODO:  Build HTML tags around  content given in the block
   #        The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+ if attr == nil
+  "<#{tag_name}>#{yield}</#{tag_name}>"
+ else
+  "<#{tag_name} #{attr[0]}='#{attr[1]}'>#{yield}</#{tag_name}>"
+ end
 end
+
+
+# print tag("h1") { "Some Title" }
+
+# y = tag("h1", ["id", "title"]) do
+#   "Some Title"
+# end
+
+# p y
 
 def timer_for
   # TODO:  Return time taken to execute the given block
+start_time =Time.now
+yield
+Time.now - start_time
 end
+
+duration = timer_for do
+end
+
+
+
+
