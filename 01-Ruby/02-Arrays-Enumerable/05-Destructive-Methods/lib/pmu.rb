22adf96ee90c04ed34cf9d778a28d4c553c74a78
MohamedTab <m.tabib@live.fr>
2014-10-10 09:16:34 +0200

    exo pmu & beautifulmycode

--- 01-Ruby/02-Arrays-Enumerable/05-Destructive-Methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/05-Destructive-Methods/lib/pmu.rb
@@ -1,5 +1,9 @@
 def pmu_format!(race_array)
   # TODO: modify the given array so that it is PMU-consistent. This method should return nil.
+  pmu_format!.each_with_index do |racer, position|
+
+
+
 end
 
 
--- 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
+++ 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
@@ -1,10 +1,7 @@
 def shuffle_word(a_word)
   # TODO: refactor this method
 
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
+  shuffle_array = a_word.upcase.chars.to_a.shuffle
 
   shuffle_array
 
@@ -14,5 +11,6 @@ end
 def quote_prime_numbers(n)
   # TODO: refactor this method
 
-  (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+  a = (1..n).find_all {|i| (2..i-1)}.select {|k| i % k == 0 }.count == 0 }
+    a = a.map{ |prime_num| "#{prime_num} is prime"}
 end
