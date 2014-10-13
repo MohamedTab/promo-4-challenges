e088b439b5402ee6afe482505650ab3190e201c3
MohamedTab <m.tabib@live.fr>
2014-10-13 17:58:13 +0200

    exo joke

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -1,5 +1,16 @@
 # Encoding: utf-8
-
-def mail_joke(email)
-  # TODO: Return (not print!) a joke suited to the email provided
+def mail_joke(email)  if email =~ /^(\S+)@(\S+)$/
+   element = email.match(/^(\S+)@(\S+)$/)
+   if element[2] == "lewagon.org"
+     return "Well done #{element[1].gsub(/\./," ")}, you're skilled and capable"
+   elsif element[2] == "gmail.com"
+     return "#{element[1].gsub(/\./," ")}, you're an average but modern person"
+   elsif element[2] == "live.com"
+     return "#{element[1].gsub(/\./," ")}, aren't you born before 1973?"
+   else
+     return "Sorry #{element[1].gsub(/\./," ")}, we don't know how to judge '#{element[2]}'"
+   end
+ else
+   raise ArgumentError
+ end
 end
