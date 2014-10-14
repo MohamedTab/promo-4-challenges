c6a1807e588ba476090bfd9bf384af5df88d9f34
MohamedTab <m.tabib@live.fr>
2014-10-14 13:52:57 +0200

    exobox_office

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -5,14 +5,12 @@ def most_successfull(number, max_year, file_name)
   # TODO: return the number most successfull movies max_year
   array_movies = []
   csv_options = { col_sep: ',', quote_char: '"' }
-
   CSV.foreach(file_name, csv_options) do |row|
     array_movies << { name: row[0], year: row[1].to_i, earnings: row[2].to_i }
     array_movies = array_movies.sort { |a, b| b[:earnings] <=> a[:earnings] }
-    end
-
-    array_movies = array_movies.select { |movie| movie[:year] < max_year }
-    array_movies = array_movies[0...number]
+  end
+  array_movies = array_movies.select { |movie| movie[:year] < max_year }
+  array_movies = array_movies[0...number]
   return array_movies
 end
 
