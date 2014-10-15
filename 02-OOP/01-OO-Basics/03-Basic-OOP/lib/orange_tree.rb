8882a42a28abfba09dbc52def18d100d9e551640
MohamedTab <m.tabib@live.fr>
2014-10-15 16:10:18 +0200

    exoorange_tree

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,49 @@
 class OrangeTree
   # TODO: Implement all the specs defined in the README.md :)
+  attr_reader :height, :age, :fruits, :pick_a_fruit, :dead, :pick_a_fruit
+  def initialize
+    @height = 0
+    @age = 0
+    @pick_a_fruit = 0
+    @fruits = 0
+    @dead = false
+  end
+
+  def fruitsperyear
+    if @age <= 5
+      @fruits = 0
+    elsif @age > 5 && @age <= 10
+      @fruits = 100
+    elsif @age > 10 && @age <= 15
+      @fruits = 200
+    else
+      @fruits = 0
+    end
+  end
+
+  def pick_a_fruit!
+    @fruits -= 1
+  end
+
+  def dead?
+    if @age <= 50
+      @dead = false
+    elsif @age > 100
+      @dead = true
+    else
+      @dead = true
+    end
+  end
+
+  def one_year_passes!
+    if @age <= 10
+      @age += 1
+      @height += 1
+      fruitsperyear
+    else
+      @age += 1
+      @height = 10
+      fruitsperyear
+    end
+  end
 end
