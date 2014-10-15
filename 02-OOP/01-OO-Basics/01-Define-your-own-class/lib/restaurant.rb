c13ef95bc693a76db7243689e2bd7012ec7aa5a2
MohamedTab <m.tabib@live.fr>
2014-10-15 12:37:31 +0200

    exo_retaurant

--- 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/restaurant.rb
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/restaurant.rb
@@ -1,10 +1,17 @@
 class Restaurant
-  attr_reader :localisation
-  def initialize(type, ticket_moyen, localisation)
+  def initialize(type, ticket_moyen, opening_hour, closing_hour)
     @type = type
     @ticket_moyen = ticket_moyen
-    @localisation = localisation
+    @opening_hour = 11
+    @closing_hour = 23
+  end
+
+  def open?
+    if   opening_hour < Time.now.hour < closing_hour
+      return "The restaurant is open"
+    else
+      return "the restaurant is close"
+    end
   end
 end
 
-#
