e68976d0db5bdd4ec5750dd7f300cd010f781994
MohamedTab <m.tabib@live.fr>
2014-10-15 17:20:36 +0200

    restau

--- 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/restaurant.rb
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/restaurant.rb
@@ -1,16 +1,41 @@
 class Restaurant
-  def initialize(type, ticket_moyen, opening_hour, closing_hour)
-    @type = type
-    @ticket_moyen = ticket_moyen
-    @opening_hour = 11
-    @closing_hour = 23
+  attr_reader :opening_hour, :closing_hour, :name
+  attr_writer :name
+  def initialize(opening_hour, closing_hour, name)
+    @opening_hour = opening_hour
+    @closing_hour = opening_hour
+    @name = %w(a b c d)
+  end
+
+  def opening_hour
+    if @name == "a"
+      @opening_hour = 10
+    elsif @name == "b"
+      @opening_hour = 11
+    elsif @name == "c"
+      @opening_hour = 12
+    elsif @name == "d"
+      @opening_hour = 13
+    end
+  end
+
+  def closing_hour
+    if @name == "a"
+      @closing_hour = 20
+    elsif @name == "b"
+      @closing_hour = 22
+    elsif @name == "c"
+      @closing_hour = 21
+    elsif @name == "d"
+      @closing_hour = 23
+    end
   end
 
   def open?
     if   opening_hour < Time.now.hour < closing_hour
       return "The restaurant is open"
     else
-      return "the restaurant is close"
+      return "The restaurant is close"
     end
   end
 end
