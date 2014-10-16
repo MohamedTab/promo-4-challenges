b3f43f18868b7a5e6023b0a75f05043dbba84900
MohamedTab <m.tabib@live.fr>
2014-10-16 11:19:19 +0200

    exo_dessert_inheritance

--- 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,36 @@
 class Dessert
+
+  attr_reader :name, :calories
   def initialize(name, calories)
+    @calories = calories
+    @name = name
   end
 
   def healthy?
+    if @calories < 200
+      return true
+    else
+      false
+    end
   end
 
   def delicious?
+    true
   end
 end
 
 class JellyBean < Dessert
+  attr_reader :flavor
   def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
+  end
+
+  def delicious?
+    if @flavor == "black licorice"
+      return false
+    else
+      return true
+    end
   end
 end
