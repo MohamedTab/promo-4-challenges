84a9a7e4f4b68894d048ac2fbe429103d40de8db
MohamedTab <m.tabib@live.fr>
2014-10-16 14:57:50 +0200

    exo_restau

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -5,38 +5,35 @@ class Restaurant
     # TODO: implement constructor with relevant instance variables
     @city = city
     @name = name
-    @average_rate= 0
+    @average_rate = 0
     @rate = 0
     @c = 0
   end
   # TODO: implement #filter_by_city and #rate methods
- def rate(number)
-  @rate += number
-  @c += 1
- end
+  def rate(number)
+    @rate += number
+    @c += 1
+  end
 
   def average_rating
     @average_rate = (@rate / @c)
   end
 
-def self.filter_by_city(restaurants, city)
-  @restos = []
-  restaurants.each do |resto|
-    if resto.city == city
-     @restos << resto
-   end
-   end
-   return @restos
+  def self.filter_by_city(restaurants, city)
+    @restos = []
+    restaurants.each do |resto|
+      @restos << resto if resto.city == city
+    end
+    return @restos
+  end
 end
 
-end
-
-macdo = Restaurant.new("paris", "macdo")
-azerty = Restaurant.new("NY", "azerty")
-macdo.rate(10)
-macdo.rate(20)
-macdo.rate(20)
-p macdo.average_rating
-restaurants = [macdo, azerty]
-p Restaurant.filter_by_city(restaurants,"NY")
+# macdo = Restaurant.new("paris", "macdo")
+# azerty = Restaurant.new("NY", "azerty")
+# macdo.rate(10)
+# macdo.rate(20)
+# macdo.rate(20)
+# p macdo.average_rating
+# restaurants = [macdo, azerty]
+# p Restaurant.filter_by_city(restaurants,"NY")
 # p @restos
