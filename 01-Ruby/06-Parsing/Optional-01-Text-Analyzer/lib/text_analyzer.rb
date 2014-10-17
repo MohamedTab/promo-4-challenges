d087a42d1019637a22a9a49b8d3f0164ac2223a6
MohamedTab <m.tabib@live.fr>
2014-10-17 11:30:01 +0200

    exo_parsing_a_finir

--- 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
+++ 01-Ruby/06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
@@ -1,3 +1,7 @@
+require 'open-uri'
 def analyze(text)
   # TODO: should analyze the text, and return the result hash with all features
+file_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
+filepath = '/home/mohamed/Bureau/olivier.txt'
+file = File.open(filepath, 'r', )
 end
