18fa864102df4945e5a4e6c833f85189a8642f99
MohamedTab <m.tabib@live.fr>
2014-10-23 20:32:31 +0200

    exo_queries

--- 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
@@ -9,30 +9,29 @@ def detailed_tracks(db)
 end
 
 def stats_on(db, genre_name)
-# TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
-result = db.execute("SELECT COUNT(*),
-  AVG(milliseconds)
-  FROM tracks t
-  JOIN genres g ON (t.genre_id = g.id)
-  WHERE g.name = '#{genre_name}';").flatten
+  # TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
+  result = db.execute("SELECT COUNT(*), AVG(milliseconds)
+    FROM tracks t
+    JOIN genres g ON (t.genre_id = g.id)
+    WHERE g.name = '#{genre_name}';").flatten
 
-length = (result[1] / 60_000).round(2)
-{
-  category: genre_name,
-  number_of_songs: result[0],
-  avg_length: length
-}
+  length = (result[1] / 60_000).round(2)
+  {
+    category: genre_name,
+    number_of_songs: result[0],
+    avg_length: length
+  }
 end
 
 def top_five_artists(db, genre_name)
   # TODO: return list of top 5 rock artists
-  a = db.execute("SELECT COUNT(*), ar.name
+  db.execute("SELECT ar.name, COUNT(*)
     FROM tracks t
     JOIN albums a on (t.album_id = a.id)
     JOIN genres g ON (t.genre_id = g.id)
     JOIN artists ar ON (a.artist_id = ar.id)
-    where g.name = '#{genre_name}';").flatten
-
-  b = []
-
+    WHERE g.name = '#{genre_name}'
+    GROUP BY ar.name
+    ORDER BY COUNT(*) DESC
+    LIMIT 5;")
 end
