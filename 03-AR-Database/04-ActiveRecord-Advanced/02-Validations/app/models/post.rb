a703acacf42686222deab3e8e58f0b5f8d0cbfb8
MohamedTab <m.tabib@live.fr>
2014-10-28 14:20:09 +0100

    exo_validation_email

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates_presence_of :user, :name, :url
+  validates :name, length: { minimum: 5 }
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :url, format: { with: /\Ahttps?\:\/\/(?:www\.|)?\w+\.\w{2,3}(?:\/\w+)?/, message: "invalid url" }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ActiveRecord::Base
   has_many :posts
 
-  # TODO: Add some validation
+  # TODO: Add some, validation
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username
+  validates :email, format: { with: /\A\w+@\w+\.\w{2,3}\z/, message: "invalid email" }
+  before_validation :stripspaces
+
+  def stripspaces
+    self.email = email.lstrip.rstrip unless email.nil?
+  end
 end
+
+
+# class User < ActiveRecord::Base
+#  has_many :posts
+#  before_validation :stripspaces
+
+#  validates :username, uniqueness: true
+# enddef stripspaces
+#  self.email = email.lstrip.rstrip unless email.nil?
+# end
