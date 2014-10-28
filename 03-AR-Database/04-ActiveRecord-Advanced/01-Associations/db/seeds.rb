b924174e625c59a41eb7d32d9d4290d9132c22c5
MohamedTab <m.tabib@live.fr>
2014-10-28 11:53:38 +0100

    exo_Associations

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,19 @@
-# TODO: Write a seed
+require 'faker'
+
+# Creating 5 fake user
+5.times do
+  user = User.new(
+    username: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+  user.save
+  (5..10).to_a.sample.times do
+    user.posts.create(
+      name: Faker::Name.name,
+      url: Faker::Internet.url,
+      votes: (1..5).to_a.sample
+       )
+  end
+end
+
+
