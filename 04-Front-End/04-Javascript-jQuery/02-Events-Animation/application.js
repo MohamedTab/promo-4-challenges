d2b66c588e447b8dbdfb779c62604132801dcb3a
MohamedTab <m.tabib@live.fr>
2014-11-03 15:19:45 +0100

    exo_events animation

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,13 @@
   * TODO: add your code here!
   *
 */
+$(function() {
+  // executed when the document is ready!
+  $('#message').slideDown("slow");
+});
+
+$(function() {
+  $("#message").click(function(){
+  $(this).slideUp("slow");
+  });
+});
