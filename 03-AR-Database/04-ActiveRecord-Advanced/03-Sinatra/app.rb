83333640d00f62c019eb7ec6d5dda46c35431d32
MohamedTab <m.tabib@live.fr>
2014-11-03 10:07:46 +0100

    exo sinatra

--- 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/app.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/03-Sinatra/app.rb
@@ -11,7 +11,33 @@ set :views, proc { File.join(root, "app/views") }
 get '/' do
   # TODO: fetch posts from database.
   #       to pass them to the view, store them in an instance variable
-
+  @posts = Post.all
 
   erb :posts  # The rendered HTML is in app/views/posts.erb
+end
+
+post '/create_post' do
+  name = params["name"]
+  url = params["url"]
+
+  Post.create(name: name, url: url)
+
+  redirect("/")
+end
+
+get 'post/:post_id/voteup' do
+  id = params[:post_id]
+  @post = Post.find(id)
+
+  post.votes += 1
+  post.save
+
+  redirect("/")
+end
+
+get '/post/:id' do
+  id = params[:id]
+  @post = Post.find(id)
+
+  erb :post
 end
